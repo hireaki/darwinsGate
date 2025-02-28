230fb6787e2954fd2c9b4d50a745e77e8d125bce
JOCASS\Jocaski <tomasjdale@gmail.com>
2025-02-28 22:12:23 +0800

    player movement flip + jump button + mini map + npc

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,33 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRb;
     public float speed;
     public float input;
+    public SpriteRenderer spriteRenderer;
+    public float jumpForce;
+
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    public Transform feetPosition;
+    public float groundCheckCircle;
 
     // Update is called once per frame
     void Update()
     {
-        input = Input.GetAxisRaw("Horizontal"); 
+        input = Input.GetAxisRaw("Horizontal");
+        if (input < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (input > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckCircle, groundLayer);
+
+        if (isGrounded == true && Input.GetButton("Jump"))
+        {
+            playerRb.velocity = Vector2.up * jumpForce;
+        }
     }
 
     void FixedUpdate()
