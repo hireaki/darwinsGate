3f7c87a2273900e62b5eb000ada55948685baf50
JOCASS\Jocaski <tomasjdale@gmail.com>
2025-03-01 21:37:11 +0800

    test

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,9 +6,11 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform playerTransfrom;
     public Vector3 offset;
-
+    public float smoothSpeed = 0.125f;
     private void LateUpdate()
     {
-        transform.position = playerTransfrom.position + offset;       
+        Vector3 desiredPosition = playerTransfrom.position + offset;
+        Vector3 smootherPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smootherPosition;      
     }
 }
