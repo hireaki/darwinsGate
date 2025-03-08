9e44e2d3351241f20ff2050bcb325dfeb099f27a
JOCASS\Jocaski <tomasjdale@gmail.com>
2025-03-09 01:01:07 +0800

    AHHHHHHHHHHHHHHH

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,120 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
-    public GameObject dialoguePanel;
-    public TextMeshProUGUI dialogueText;
-    public string[] dialogue;
-    private int index = 0;
+    // UI References
+    [SerializeField]
+    private TMP_Text speakerText;
 
-    public GameObject contButton;
-    public float wordSpeed;
-    public bool playerIsClose;
+    [SerializeField]
+    private TMP_Text dialogueText;
 
-    public AudioSource audioSource;
-    public AudioClip dialogueSound;
+    [SerializeField]
+    private Image portraitImage;
 
-    void Start()
-    {
-        dialogueText.text = "";
-        if (audioSource != null)
-        {
-            audioSource.loop = true; // Make it loop while typing
-        }
-    }
+    //Dialogue Content
+    [SerializeField]
+    private string[] speaker;
 
+    [SerializeField]
+    [TextArea]
+    private string[] dialogueWords;
+
+    [SerializeField]
+    private Sprite[] portrait;
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        if (Input.GetButtonDown("Interact"))
         {
-            if (!dialoguePanel.activeInHierarchy)
-            {
-                dialoguePanel.SetActive(true);
-                StartCoroutine(Typing());
-            }
-            else if (dialogueText.text == dialogue[index])
-            {
-                NextLine();
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Q) && dialoguePanel.activeInHierarchy)
-        {
-            RemoveText();
-        }
-
-        if (dialogueText.text == dialogue[index])
-        {
-            contButton.SetActive(true);
-        }
-    }
-
-    public void RemoveText()
-    {
-        dialogueText.text = "";
-        index = 0;
-        dialoguePanel.SetActive(false);
-        StopSound(); // Stop sound when dialogue is removed
-    }
-
-    IEnumerator Typing()
-    {
-        if (audioSource != null && dialogueSound != null)
-        {
-            audioSource.clip = dialogueSound;
-            audioSource.Play(); // Start playing the sound
-        }
-
-        foreach (char letter in dialogue[index].ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(wordSpeed);
-        }
-
-        StopSound(); // Stop sound when typing is finished
-    }
-
-    public void NextLine()
-    {
-        contButton.SetActive(false);
-
-        if (index < dialogue.Length - 1)
-        {
-            index++;
-            dialogueText.text = "";
-            StartCoroutine(Typing());
-        }
-        else
-        {
-            RemoveText();
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerIsClose = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerIsClose = false;
-            RemoveText();
-        }
-    }
-
-    private void StopSound()
-    {
-        if (audioSource != null && audioSource.isPlaying)
-        {
-            audioSource.Stop(); // Stop the sound when typing is done
+            speakerText.text = speaker[0];
+            dialogueText.text = dialogueWords[0];
+            portraitImage.sprite = portrait[0];
         }
     }
 }
